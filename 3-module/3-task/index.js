ea97c4c5dfae83eb92da1e1d890b6affd39dfa99
Kirill Kostenok <kostenokkirill@gmail.com>
2022-09-12 23:14:42 +0300

    решение задачи 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+      function toUpper(element, index, array) {
+        if (index != 0) array[index] = element[0].toUpperCase() + element.slice(1);
+      }
+      arr.forEach(toUpper);
+      let result = arr.join("");
+      return result;
 }
