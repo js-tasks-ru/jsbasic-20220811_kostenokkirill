eefdaeb41a54faf0961076fbfd057e130cd6cf76
Kirill Kostenok <kostenokkirill@gmail.com>
2022-09-12 23:34:25 +0300

    решение задачи 3.4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let someUsers = users.filter(item => item.age <= age);
+  let someUsersCut = someUsers.map(item => (item.name + ', ' + item.balance));
+  let string = '';
+  someUsersCut.forEach((item, index, array) => {
+    string = string + item;
+    if (index < array.length - 1) string = string + '\n';
+  })
+  return string;
 }
