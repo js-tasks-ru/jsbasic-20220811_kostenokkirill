90850c6db9d8a0f1a5de95754f001c70b59962f2
Kirill Kostenok <kostenokkirill@gmail.com>
2022-09-13 20:45:37 +0300

    решение задачи 4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,12 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.children[1].children) {
+    if (tr.lastElementChild.dataset.available == null) tr.hidden = true;
+    if (tr.lastElementChild.dataset.available == 'true') tr.classList.add('available');
+    if (tr.lastElementChild.dataset.available == 'false') tr.classList.add('unavailable');
+    if (tr.children[2].innerHTML == 'm') tr.classList.add('male');
+      else if (tr.children[2].innerHTML == 'f') tr.classList.add('female');
+    if (Number(tr.children[1].innerHTML) < 18) {
+            tr.style = "text-decoration: line-through";
+    }
+  }    
 }
